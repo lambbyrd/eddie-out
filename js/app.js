664a9500c7596e980c356ca32d4e27d97060c205
Chris Klanac <cklanac@users.noreply.github.com>
2016-08-07 19:42:10 -0400

    reorganize functions into logical objects

--- js/app.js
+++ js/app.js
@@ -1,70 +1,61 @@
-//google maps api: AIzaSyDeEhJNVidPVfBn1WRHALIczTr2fEQpytQ
-
-var RiverMap = {
-
-    riverLocations: [],
-
-    getUsgsData: function() {
-
-        var url = 'http://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=ca&parameterCd=00060&siteType=ST&siteStatus=active&altMin=130';
-
-        $.getJSON(url, function(data, status, xhr) {
-
-            console.log(data.value.timeSeries);
-            RiverMap.convertUsgsData(data.value.timeSeries);
-
-        });
-
-    },
-
-    convertUsgsData: function(data) {
-        //console.log(data);
-        for (var i = 0; i < data.length; i++) {
-        	
-            RiverMap.riverLocations.push({ 
-            	name: data[i].sourceInfo.siteName,
-             	lat: data[i].sourceInfo.geoLocation.geogLocation.latitude, 
-             	lng: data[i].sourceInfo.geoLocation.geogLocation.longitude,
-             	flow: data[i].values[0].value[0].value });
-
-            RiverMap.addRiverTag(RiverMap.riverLocations[i].lat, RiverMap.riverLocations[i].lng, RiverMap.riverLocations[i].name);
-        }
-
-    }
-}
-var map;
-
-RiverMap.gMapInit = function() {
-
-    var mapOptions = {
+// google maps api: AIzaSyDeEhJNVidPVfBn1WRHALIczTr2fEQpytQ
+// gMap contains the google maps related functions
+// It is a global variable, but needs to be since gMap.init is callback for the google maps api 
+var gMap = {
+    map: null, 
+    mapElement: $('#map-area')[0],
+    mapOptions: {
         zoom: 5,
         center: {
             lat: 39.09024,
-            lng: -92.712891
+            lng: -112.712891
         }
-    };
-
-    map = new google.maps.Map(document.getElementById('map-area'), mapOptions);
+    },
+    init: function () {
+        //initialize map
+        gMap.map = new google.maps.Map(this.mapElement, this.mapOptions);
+    },
+    /*Add Maker to maps*/
+    //put addTag here since it is more map related than data related
+    addTag: function (lat, lng, name) {
+        var latLng = new google.maps.LatLng(lat, lng);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            title: name
+        });
+        marker.setMap(this.map);
+    }
 };
 
-/*Add Maker to maps*/
-RiverMap.addRiverTag = function(lat, lng, name) {
+var usgs = {
+    url: 'http://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=ca&parameterCd=00060&siteType=ST&siteStatus=active&altMin=130',
+    riverLocations: [],
+    getData: function () {
+        var that = this;
+        return $.getJSON(this.url, function (data, status, xhr) {
+            that.convertData(data.value.timeSeries);
+        });
+    },
+    convertData: function (data) {
+        for (var i = 0; i < data.length; i++) {
+            usgs.riverLocations.push({
+                name: data[i].sourceInfo.siteName,
+                lat: data[i].sourceInfo.geoLocation.geogLocation.latitude,
+                lng: data[i].sourceInfo.geoLocation.geogLocation.longitude,
+                flow: data[i].values[0].value[0].value
+            });
+        }
+    }
+};
 
-    var latLng = new google.maps.LatLng(
-        lat, lng);
-
-    var marker = new google.maps.Marker({
-        position: latLng,
-        title: name
+// document.ready should run only after google maps loads and runs the init
+$(function () {
+    // on document.ready we call usgs.getDataajax call
+    // when it returns, then update the map with the data
+    var promise = usgs.getData();
+    promise.done(function () {
+        usgs.riverLocations.forEach(function (obj) {
+            gMap.addTag(obj.lat, obj.lng, obj.name);
+        });
     });
-
-    marker.setMap(map);
-};
-
-
-$(function() {
-    //google maps init
-    RiverMap.getUsgsData();
-    //addRiverTag(RiverMap.riverLocations[0].lat,RiverMap.riverLocations[0].lng);
-
 });
