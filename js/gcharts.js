3a21c58841c0886b895f66a71fd0df6d616223b3
Codepressd <creederone@gmail.com>
2016-08-29 13:04:28 -0700

    tried to use webpack but it broke the site

---
+++ js/gcharts.js
@@ -0,0 +1,66 @@
+/*Google Charts Javascript*/
+
+var gCharts = {
+
+    init: function() {
+        google.charts.load('current', { 'packages': ['corechart', 'gauge'] });
+
+    },
+
+    /*Draws the area chart*/
+
+    doDrawAreaChart: function(site) {
+        google.charts.setOnLoadCallback(drawAreaChart(site));
+
+        function drawAreaChart(site) {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Date', 'Flow'],
+                [site.flow[0][0], site.flow[0][1]],
+                [site.flow[1][0], site.flow[1][1]],
+                [site.flow[2][0], site.flow[2][1]]
+            ]);
+
+            var options = {
+                title: site.name,
+                backgroundColor: '#EAEAEA',
+                hAxis: { title: 'Date', titleTextStyle: { color: '#333' } },
+                vAxis: { minValue: 0 }
+            };
+
+            var chart = new google.visualization.AreaChart($('.chart')[0]);
+            chart.draw(data, options);
+        }
+    },
+
+    /*Draws the Gauge*/
+
+    doDrawGauge: function(site) {
+        google.charts.setOnLoadCallback(drawGauge(site));
+
+        function drawGauge(site) {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['River CFS', site.flow[0][1]]
+            ]);
+
+            var options = {
+                width: '100%',
+                height: '100%',
+                redFrom: 900,
+                redTo: 1000,
+                yellowFrom: 700,
+                yellowTo: 900,
+                minorTicks: 10
+            };
+
+            var chart = new google.visualization.Gauge($('.gauge')[0]);
+
+            chart.draw(data, options);
+
+        }
+    }
+}
+
+module.exports = gCharts;
---
+++ js/gmaps.js
@@ -0,0 +1,59 @@
+/*Google Maps Javascript File*/
+
+var index = null;
+/*Google Maps*/
+var gMap = {
+
+    markers: [],
+
+    map: null,
+
+    mapArea: $('#map-area')[0],
+
+    mapOptions: {
+        zoom: 5,
+        center: {
+            lat: 39.09024,
+            lng: -92.712891
+        }
+    },
+
+    init: function() {
+
+        this.map = new google.maps.Map(this.mapArea, this.mapOptions);
+    },
+
+    /*adds tags/markers to the map*/
+    addRiverTag: function(lat, lng, name, flow) {
+        var image = './img/raft-icon.png';
+
+        var latLng = new google.maps.LatLng(lat, lng);
+
+        var marker = new google.maps.Marker({
+            position: latLng,
+            title: name,
+            icon: image
+        });
+
+        marker.setMap(this.map);
+        gMap.markers.push(marker);
+
+        /*
+        Adds a listener to the markers and pops up information about the specific site
+        Soon I will be removing this portion from this object.
+
+        */
+        marker.addListener('click', function() {
+            var index = gMap.markers.indexOf(this);
+
+            $('.lightbox').addClass('showLightbox');
+
+            gCharts.doDrawAreaChart(usgs.filteredLocations[index]);
+            gCharts.doDrawGauge(usgs.filteredLocations[index]);
+            
+
+        })
+    },
+}
+
+module.exports = gMap;
